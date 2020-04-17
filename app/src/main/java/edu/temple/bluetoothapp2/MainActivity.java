b1863c5166bd024d7a58a3139d3e95a269dcd9e9
Dishant Patel <tug02945@temple.edu>
2020-04-17 14:17:22 -0400

    beAcon didnt work so added btadapter

--- app/src/main/java/edu/temple/bluetoothapp2/MainActivity.java
+++ app/src/main/java/edu/temple/bluetoothapp2/MainActivity.java
@@ -1,17 +1,59 @@
 package edu.temple.bluetoothapp2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Beacon beacon;
+    private BeaconManager bManager;
+    private BluetoothAdapter BTAdapter;
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    private static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Beacon beacon;
+        //
+
+        BTAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Phone does not support Bluetooth so let the user know and exit.
+        if (BTAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .show();
+        }
+        Button scanBtn = findViewById(R.id.button);
+        textview = findViewById(R.id.textView);
+        
     }
+
 }
